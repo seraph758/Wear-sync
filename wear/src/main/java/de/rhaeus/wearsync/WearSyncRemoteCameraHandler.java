7cb7921fd0468b600f58054da9a18c1d042d7402
seraph758 <luke.likai@gmail.com>
2026-06-23 09:37:12 +0800

    Update WearSyncRemoteCameraHandler.java

--- wear/src/main/java/de/rhaeus/wearsync/WearSyncRemoteCameraHandler.java
+++ wear/src/main/java/de/rhaeus/wearsync/WearSyncRemoteCameraHandler.java
@@ -1,5 +1,6 @@
 package de.rhaeus.wearsync;
 
+
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -7,60 +8,103 @@ import android.util.Log;
 
 import androidx.wear.remote.interactions.RemoteActivityHelper;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+
 
 public class WearSyncRemoteCameraHandler {
 
-    private static final String TAG = "WearSync_RemoteCamera";
+
+    private static final String TAG =
+            "WearSync_RemoteCamera";
 
 
-    public static void openPhoneCamera(Context context) {
+    private final Context context;
 
 
-    Log.d(TAG,"开始调用 RemoteActivityHelper");
+    private final Executor executor =
+            Executors.newSingleThreadExecutor();
 
 
-    Intent intent = new Intent(
-            Intent.ACTION_VIEW
-    );
+
+    public WearSyncRemoteCameraHandler(Context context){
+
+        this.context = context.getApplicationContext();
+
+    }
 
 
-    intent.setPackage(
-            "de.rhaeus.wearsync"
-    );
 
 
-    intent.setData(
-            Uri.parse(
-                    "wearsync://camera"
-            )
-    );
+
+    public void openPhoneCamera(){
 
 
-    Log.d(
-            TAG,
-            "Intent="
-            + intent.toUri(0)
-    );
+        try {
 
 
-    RemoteActivityHelper helper =
-            new RemoteActivityHelper(
-                    context
+            RemoteActivityHelper helper =
+                    new RemoteActivityHelper(
+                            context,
+                            executor
+                    );
+
+
+
+            Intent intent =
+                    new Intent(Intent.ACTION_VIEW);
+
+
+
+            intent.setPackage(
+                    "de.rhaeus.wearsync"
             );
 
 
-    helper.startRemoteActivity(intent)
-            .addListener(() -> {
 
-                Log.d(
-                        TAG,
-                        "RemoteActivity finished"
-                );
-
-            },
-            Executors.newSingleThreadExecutor());
+            intent.setData(
+                    Uri.parse(
+                            "wearsync://camera"
+                    )
+            );
 
 
-}
+
+            Log.d(
+                    TAG,
+                    "发送RemoteActivity请求"
+            );
+
+
+
+            helper.startRemoteActivity(
+                    intent
+            ).addListener(
+                    () -> {
+
+                        Log.d(
+                                TAG,
+                                "RemoteActivity执行完成"
+                        );
+
+                    },
+                    executor
+            );
+
+
+        }catch(Exception e){
+
+
+            Log.e(
+                    TAG,
+                    "RemoteActivity失败",
+                    e
+            );
+
+
+        }
+
+
+    }
+
 }
