6f0e6532d2f313adc76306397dd41318fd6a3d89
seraph758 <luke.likai@gmail.com>
2026-06-23 16:38:29 +0800

    更新 WearSyncRemoteCameraActivity.java

--- mobile/src/main/java/de/rhaeus/wearsync/WearSyncRemoteCameraActivity.java
+++ mobile/src/main/java/de/rhaeus/wearsync/WearSyncRemoteCameraActivity.java
@@ -15,12 +15,16 @@ public class WearSyncRemoteCameraActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-
-        // ===== 调试日志 =====
         Log.e(
                 TAG,
-                "★★★★★★★★ WearSyncRemoteCameraActivity 已启动 ★★★★★★★★"
+                "① onCreate开始执行"
+        );
+
+        super.onCreate(savedInstanceState);
+
+        Log.e(
+                TAG,
+                "② super.onCreate执行完成"
         );
 
         Toast.makeText(
@@ -29,10 +33,26 @@ public class WearSyncRemoteCameraActivity extends Activity {
                 Toast.LENGTH_LONG
         ).show();
 
-        // ===== 原有逻辑 =====
-        Log.d(
+        Log.e(
                 TAG,
-                "收到RemoteActivity相机请求"
+                "③ Toast已显示"
+        );
+
+        Log.e(
+                TAG,
+                "④ 当前线程="
+                        + Thread.currentThread().getName()
+        );
+
+        Log.e(
+                TAG,
+                "⑤ 当前Intent="
+                        + getIntent()
+        );
+
+        Log.e(
+                TAG,
+                "⑥ 准备启动 PhoneSyncCameraService"
         );
 
         Intent serviceIntent =
@@ -41,20 +61,115 @@ public class WearSyncRemoteCameraActivity extends Activity {
                         PhoneSyncCameraService.class
                 );
 
+        Log.e(
+                TAG,
+                "⑦ Service Intent创建完成"
+        );
+
         serviceIntent.setAction(
                 PhoneSyncCameraService.ACTION_START_CAMERA
         );
 
-        if (Build.VERSION.SDK_INT >= 26) {
+        Log.e(
+                TAG,
+                "⑧ Action="
+                        + PhoneSyncCameraService.ACTION_START_CAMERA
+        );
 
-            startForegroundService(serviceIntent);
+        try {
 
-        } else {
+            if (Build.VERSION.SDK_INT >= 26) {
 
-            startService(serviceIntent);
+                Log.e(
+                        TAG,
+                        "⑨ 调用 startForegroundService"
+                );
 
+                startForegroundService(
+                        serviceIntent
+                );
+
+            } else {
+
+                Log.e(
+                        TAG,
+                        "⑨ 调用 startService"
+                );
+
+                startService(
+                        serviceIntent
+                );
+
+            }
+
+            Log.e(
+                    TAG,
+                    "⑩ Service启动调用完成"
+            );
+
+        } catch (Exception e) {
+
+            Log.e(
+                    TAG,
+                    "❌ Service启动异常",
+                    e
+            );
         }
 
+        Log.e(
+                TAG,
+                "⑪ 即将finish"
+        );
+
         finish();
+
+        Log.e(
+                TAG,
+                "⑫ finish已调用"
+        );
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        Log.e(
+                TAG,
+                "onStart"
+        );
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+
+        Log.e(
+                TAG,
+                "onResume"
+        );
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+
+        Log.e(
+                TAG,
+                "onPause"
+        );
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        Log.e(
+                TAG,
+                "onDestroy"
+        );
+
+        super.onDestroy();
     }
 }
