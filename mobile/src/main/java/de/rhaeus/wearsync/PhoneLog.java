df308fb30511bd1bf7500b45fa726135d9d48f3f
seraph758 <luke.likai@gmail.com>
2026-06-24 18:30:53 +0800

    Update PhoneLog.java

--- mobile/src/main/java/de/rhaeus/wearsync/PhoneLog.java
+++ mobile/src/main/java/de/rhaeus/wearsync/PhoneLog.java
@@ -4,11 +4,10 @@ import android.util.Log;
 
 /**
  * 📲 手機端專用日誌管理器
- * 作用：一鍵控制手機端所有自定義排查日誌的開啟與關閉
  */
 public class PhoneLog {
-    // 🌟 核心開關：調試時保持 true；發布正式版時改為 false 全局靜默
-    public static final boolean DEBUG = true; 
+    // 🌟 去掉了 final，現在它是一個可以被界面按鈕動態撥動的開關了！
+    public static boolean DEBUG = true; 
 
     public static void d(String tag, String msg) {
         if (DEBUG) {
@@ -23,7 +22,6 @@ public class PhoneLog {
     }
 
     public static void e(String tag, String msg) {
-        // 錯誤日誌建議無論何時都保持輸出，方便線上抓崩潰
         Log.e(tag, msg);
     }
 
@@ -31,4 +29,3 @@ public class PhoneLog {
         Log.e(tag, msg, tr);
     }
 }
-
