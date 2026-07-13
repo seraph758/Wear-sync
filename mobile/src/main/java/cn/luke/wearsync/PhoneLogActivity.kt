b4241975f5bc36f3f1c27d1f7b7c823e8e6ccff9
seraph758 <luke.likai@gmail.com>
2026-07-13 10:22:41 +0800

    更新 PhoneLogActivity.kt

--- mobile/src/main/java/cn/luke/wearsync/PhoneLogActivity.kt
+++ mobile/src/main/java/cn/luke/wearsync/PhoneLogActivity.kt
@@ -28,8 +28,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
-import androidx.compose.material.icons.filled.ContentCopy
-import androidx.compose.material.icons.filled.Save
+
 
 
 @OptIn(ExperimentalFoundationApi::class)
